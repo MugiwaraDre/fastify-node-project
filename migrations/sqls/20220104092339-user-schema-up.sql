416220ca8cb7694560e6c38a830442aae29b59ea
MugiwaraDre <atd.hirsch@gmail.com>
2022-01-04 22:15:47 +0100

    [Dre] | test user service fix 2

--- migrations/sqls/20220104092339-user-schema-up.sql
+++ migrations/sqls/20220104092339-user-schema-up.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   first_name VARCHAR(100) NOT NULL,
   middle_name VARCHAR(100),
-  last_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100),
   password VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL,
   created_at timestamptz NOT NULL DEFAULT now(),
